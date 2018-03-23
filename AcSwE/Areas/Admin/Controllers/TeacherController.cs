eecb5edb180cbb8b83feec22b159393d5aa7c329
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-03-23 09:11:56 +0700

    area admin

---
+++ AcSwE/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AcSwE.Models;
+
+namespace AcSwE.Areas.Admin.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Teacher
+        public ActionResult Index()
+        {
+            return View(db.Teachers.ToList());
+        }
+
+        // GET: Admin/Teacher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // GET: Admin/Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+                
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HttpPostedFileBase file, [Bind(Include = "id,title,firstName,lastName,username,password,img")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                file.SaveAs(HttpContext.Server.MapPath("~/Content/img/teacher/")
+                                  + file.FileName);
+                teacher.img = file.FileName;
+                db.Teachers.Add(teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(teacher);
+        }
+
+        // GET: Admin/Teacher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Admin/Teacher/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,title,firstName,lastName,username,password,img")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(teacher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        // GET: Admin/Teacher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Admin/Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            db.Teachers.Remove(teacher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
