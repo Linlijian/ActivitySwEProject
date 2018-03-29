02b77c750cce85890dac1759860cbf7ef3ef39f2
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-03-29 19:10:10 +0700

    login + logout ex.

--- AcSwE/Controllers/HomeController.cs
+++ AcSwE/Controllers/HomeController.cs
@@ -1,30 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using AcSwE.Models;
 
 namespace AcSwE.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Login(Teacher t)
         {
-            ViewBag.Message = "Your contact page.";
+            using (db)
+            {
+                //var user = db.Teachers.Where(x => x.username == t.username && x.password == t.password).FirstOrDefault();
+                var user = db.Teachers.Where(x => x.username == t.username).FirstOrDefault();
+                var userid = user.id;
+                if (user == null)
+                {
+                    ViewBag.user = "Wrong username or password";
+                    return View(user);
+                }
+                else
+                {
+                    Session["user"] = user.firstName;
+                    Session["id"] = user.id;
+                    return RedirectToAction("Index");
+                }
+            }
+                
+        }
 
-            return View();
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+       
+            public ActionResult Details()
+        {
+            
+            return RedirectToAction("Details","Teacher");
         }
     }
 }
