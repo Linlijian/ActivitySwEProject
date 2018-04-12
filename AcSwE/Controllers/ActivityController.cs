0f26cf3872840013e2da5f5a8cd08e8ebf98519e
linlijian <praw__Rul2__lvluk@outlook.co.th>
2018-04-12 21:35:44 +0700

    index view with out login

---
+++ AcSwE/Controllers/ActivityController.cs
@@ -0,0 +1,65 @@
+﻿using AcSwE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcSwE.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Activity
+        public ActionResult Index()
+        {
+            return View(db.Activitys.ToList());
+        }
+
+        // GET: Activity/Details/5
+        public ActionResult Details(int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activitys.Find(id);
+            var data = (from a in db.Joins where a.idActivity == id select a).ToList();
+            StudentTemp t = new StudentTemp();
+            for (int i = 0; i < data.Count(); i++)
+            {
+                t.idStd = data[i].idStd;
+                db.StudentTemps.Add(t);
+                db.SaveChanges();
+            }
+
+            ViewBag.std = (from q in db.StudentTemps join w in db.Students on q.idStd equals w.idStd select w).ToList();
+
+            ViewBag.acid = id;
+
+            var temp = db.StudentTemps.ToList();
+            StudentTemp std = new StudentTemp();
+            for (int i = 0; i < temp.Count(); i++)
+            {
+                std = db.StudentTemps.Find(temp[i].id);
+                db.StudentTemps.Remove(std);
+                db.SaveChanges();
+            }
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+        public ActionResult DetailsStd(int? id, int Acid)
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority +
+            Request.ApplicationPath.TrimEnd('/') + "/" + "Activity/Details/" + Acid;
+            return RedirectToAction("Details", "Studented", new { id, baseUrl });
+        }
+
+
+
+    }
+}
---
+++ AcSwE/Controllers/StudentedController.cs
@@ -0,0 +1,73 @@
+﻿using AcSwE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcSwE.Controllers
+{
+    public class StudentedController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Student
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Details(string baseUrl, int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            ViewBag.count = (from a in db.Joins where student.idStd == a.idStd select a).Count();
+            var data = (from s in db.Joins where s.idStd == student.idStd select s).ToList();
+            StudentTemp t = new StudentTemp();
+            for (int i = 0; i < data.Count(); i++)
+            {
+                t.idStd = data[i].idActivity;
+                db.StudentTemps.Add(t);
+                db.SaveChanges();
+            }
+            ViewBag.ST_AC = (from z in db.StudentTemps join x in db.Activitys on z.idStd equals x.id select x).ToList();
+
+            //del
+            var temp = db.StudentTemps.ToList();
+            StudentTemp std = new StudentTemp();
+            for (int i = 0; i < temp.Count(); i++)
+            {
+                std = db.StudentTemps.Find(temp[i].id);
+                db.StudentTemps.Remove(std);
+                db.SaveChanges();
+            }
+
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.url = baseUrl;
+            //if (baseUrl != null)
+            //    return Redirect(baseUrl);           
+            return View(student);
+        }
+
+        public ActionResult CallDetails(int? id)
+        {
+            return RedirectToAction("Details", "Activity", new { id });
+        }
+
+        [HttpPost]
+        public ActionResult callBack(string baseUrl)
+        {
+            if (baseUrl != null)
+                return Redirect(baseUrl);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
